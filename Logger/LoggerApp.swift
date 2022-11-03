37ad3563a67a16be429945f148fd0693a95c64be
jakub.kucmin <jakub.kucmin@iteo.com>
2022-11-03 10:48:52 +0100

    Created persistance service

--- Logger/LoggerApp.swift
+++ Logger/LoggerApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @main
 struct LoggerApp: App {
-    let persistenceController = PersistenceController.shared
+    let persistenceController = PersistenceServiceImpl.shared
 
     var body: some Scene {
         WindowGroup {
